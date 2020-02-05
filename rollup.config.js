c436aa0d530332505f1c4c65527c4f45b6725c2b
davidcai <w.david.cai@gmail.com>
2020-02-05 14:20:46 -0800

    ci: setup rollup (#4)

---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import typescript from "rollup-plugin-typescript2";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      format: "cjs",
+      dir: "dist",
+      sourcemap: true
+    },
+    {
+      format: "esm",
+      file: "dist/state-store.esm.js",
+      sourcemap: true
+    }
+  ],
+  external: ["react", "immer"],
+  plugins: [typescript()]
+};
